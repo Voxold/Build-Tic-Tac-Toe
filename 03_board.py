4fdb0bc369c1d9233845134d49b85588f123dd6a
Voxold <bilalajaha@gmail.com>
2024-02-16 01:12:19 +0100

    board

---
+++ 03_board.py
@@ -0,0 +1,18 @@
+class Board():
+    def __init__(self):
+        self.board = [str(i) for i in range (1,10)]
+
+    def display_board(self):
+        for i in range(0,9,3):
+            print(' | '.join(self.board[i:i+3]))
+            if i < 6:
+                print('-'*10)
+        
+    
+    def update_board(self):
+        pass
+
+    def reset_board(self):
+        pass
+
+lis = [1,2,3,4,5,6,7,8,9]
