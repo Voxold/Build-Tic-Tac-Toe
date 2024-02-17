d2091180d947db23210f12364aa6f683aca1bbac
Voxold <bilalajaha@gmail.com>
2024-02-17 15:41:30 +0100

    menu

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-class Menu():
+class Menu:
     """We havn't __init__ cuz we dont have attribuets"""
     def main_menu(self):
         main_menu = """Welcome to X-O game
@@ -10,8 +10,7 @@ class Menu():
                 return choice
                 break
             print('Invalide, please choose between 1 and 2 :')
-        
-                
+
     def end_menu(self):
         end_menu =  """Welcome to X-O game
                         1 - Start Game
